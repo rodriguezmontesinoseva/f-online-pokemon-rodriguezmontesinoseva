8a6c595ef4ea4731eac3e3d9cd75ef9a4144c568
Eva Rodriguez <rodriguezmontesinoseva@gmail.com>
2019-06-22 09:08:14 +0200

    fix fetch

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import PokeList from './PokeList';
 
 
 //const ENDPOINT = '../../objPokemon.json'
-const ENDPOINT = 'http://localhost:4000/results';
+//const ENDPOINT = 'http://localhost:4000/results';
 
 class App extends Component {
 	constructor(props) {
@@ -20,34 +20,36 @@ class App extends Component {
 
 	fetchPokemon() {
 		//console.log('entra en el fetch');
-		//fetch('https://pokeapi.co/api/v2/pokemon?limit=25/')
-		fetch(ENDPOINT)
+		fetch('https://pokeapi.co/api/v2/pokemon?limit=25/')
+			//fetch(ENDPOINT)
 			.then(response => response.json())
 			.then(data => {
-				console.log(data);
+				//	console.log(data);
 				const resultsPk = data.results;
-				this.state.isFetching ? console.log('cargando') :
-					resultsPk.map(element => {
-						fetch(element.url)
-							.then(response => response.json())
-							.then(detailPk => {
-								this.setState(prevState => {
-									return {
-										pokData: [
-											...prevState.pokData,
-											{
-												name: detailPk.name,
-												types: detailPk.types,
-												img: detailPk.sprites.front_default,
-												id: detailPk.id
-											}
-										],
-										isFetching: false
-									}
-								})
+				//this.state.isFetching ? console.log('cargando') :
+				resultsPk.map(element => {
+					fetch(element.url)
+						.then(response => response.json())
+						.then(detailPk => {
+							this.setState(prevState => {
+								return {
+									pokData: [
+										...prevState.pokData,
+										{
+											name: detailPk.name,
+											types: detailPk.types,
+											img: detailPk.sprites.front_default,
+											id: detailPk.id
+										}
+									],
+									isFetching: false,
+
+								}
 							})
-					})
-					;
+
+						})
+				})
+				//	;
 				// this.setState({
 				// 	pokData: data.results,
 				// 	isFetching: false
@@ -55,6 +57,8 @@ class App extends Component {
 
 
 			})
+
+
 	}
 
 
@@ -62,15 +66,19 @@ class App extends Component {
 
 	componentDidMount() {
 		this.fetchPokemon();
+		//	console.log(`esto es pokdata ${this.state.pokData}`);
 	}
 
 	render() {
 		return (
 			<div>
 				<h1 className="title">Mi lista de Pokemon</h1>
-				{this.state.isFetching ? <p>loading...</p> : <p>array cargado</p>}
 
-				{/* <PokeList arrPokemon={ENDPOINT} /> */}
+				{this.state.isFetching ? <p>loading...</p> : <p>array cargado</p>}
+				{console.dir(this.state.pokData)}
+
+
+				{/* <PokeList pokData={this.state.pokData} />  */}
 			</div>
 		);
 	}
