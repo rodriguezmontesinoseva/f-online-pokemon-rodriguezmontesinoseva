529c4b61ef8f00b497b6580a55eab7b00ae2cfbd
Eva Rodriguez <rodriguezmontesinoseva@gmail.com>
2019-06-25 11:53:44 +0200

    create docs

---
+++ docs/precache-manifest.3b7608234693ee6bd7c5e90dcd2f97f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b86996bf88759df6a4a755223357aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a039d1b778e5c5a125f",
+    "url": "/static/css/main.60794438.chunk.css"
+  },
+  {
+    "revision": "c6e779d74e3dde6bad26",
+    "url": "/static/js/2.a0becd36.chunk.js"
+  },
+  {
+    "revision": "9a039d1b778e5c5a125f",
+    "url": "/static/js/main.5a728a7b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
